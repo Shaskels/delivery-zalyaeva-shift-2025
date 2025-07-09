d120b5afed16e8f024bc9fe02c6f9c251a9b6930
Shaskels <allasidner028@gmail.com>
2025-07-09 22:14:29 +0700

    refactor add separated delivery calculate view model

---
+++ app/src/main/java/com/example/delivery_zalyaeva_shift_2025/presentation/DeliveryCalculationViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.delivery_zalyaeva_shift_2025.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.delivery_zalyaeva_shift_2025.domain.usecase.CalculateCostUseCase
+
+class DeliveryOptionsViewModel(
+    private val calculateCostUseCase: CalculateCostUseCase
+): ViewModel() {
+
+
+}
