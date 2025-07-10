ee1fe3014505fc36c5fe242c9bf0771699a85de7
Shaskels <allasidner028@gmail.com>
2025-07-10 11:24:27 +0700

    style

--- app/src/main/java/com/example/delivery_zalyaeva_shift_2025/util/ListElementGetter.kt
+++ app/src/main/java/com/example/delivery_zalyaeva_shift_2025/util/ListElementGetter.kt
@@ -1,18 +1,16 @@
 package com.example.delivery_zalyaeva_shift_2025.util
 
 fun <T> List<T>.getElements(number: Int, startIndex: Int): List<T> {
+    val list = ArrayList<T>()
     if (this.size >= number){
-        val list = ArrayList<T>()
         for(i in startIndex..number) {
             list.add(this[i])
         }
-        return list
     }
     else {
-        val list = ArrayList<T>()
         for(i in startIndex..this.size) {
             list.add(this[i])
         }
-        return list
     }
+    return list
 }
